b515d64f3d39b2d9a9dedc12fff8d3d45d4a0142
Jordan Dalton <jordan.dalton@ymail.com>
2023-04-14 17:43:09 -0500

    Laravel Breeze: Intertia, Vue, Tailwind

---
+++ tests/Feature/ProfileTest.php
@@ -0,0 +1,88 @@
+<?php
+
+test('test profile page is displayed', function(){
+
+    $user = \App\Models\User::factory()->create();
+
+    $response = $this
+        ->actingAs($user)
+        ->get('/profile');
+
+    $response->assertOk();
+});
+
+test('test profile information can be updated', function(){
+
+    $user = \App\Models\User::factory()->create();
+
+    $response = $this
+        ->actingAs($user)
+        ->patch('/profile', [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+    $response
+        ->assertSessionHasNoErrors()
+        ->assertRedirect('/profile');
+
+    $user->refresh();
+
+    $this->assertSame('Test User', $user->name);
+    $this->assertSame('test@example.com', $user->email);
+    $this->assertNull($user->email_verified_at);
+});
+
+test('test email verification status is unchanged when the email address is unchanged', function(){
+
+    $user = \App\Models\User::factory()->create();
+
+    $response = $this
+        ->actingAs($user)
+        ->patch('/profile', [
+            'name' => 'Test User',
+            'email' => $user->email,
+        ]);
+
+    $response
+        ->assertSessionHasNoErrors()
+        ->assertRedirect('/profile');
+
+    $this->assertNotNull($user->refresh()->email_verified_at);
+});
+
+test('test user can delete their account', function(){
+
+    $user = \App\Models\User::factory()->create();
+
+    $response = $this
+        ->actingAs($user)
+        ->delete('/profile', [
+            'password' => 'password',
+        ]);
+
+    $response
+        ->assertSessionHasNoErrors()
+        ->assertRedirect('/');
+
+    $this->assertGuest();
+    $this->assertNull($user->fresh());
+});
+
+test('test correct password must be provided to delete account', function(){
+
+    $user = \App\Models\User::factory()->create();
+
+    $response = $this
+        ->actingAs($user)
+        ->from('/profile')
+        ->delete('/profile', [
+            'password' => 'wrong-password',
+        ]);
+
+    $response
+        ->assertSessionHasErrors('password')
+        ->assertRedirect('/profile');
+
+    $this->assertNotNull($user->fresh());
+});
