385747fda3813d9356bed44906a3b6944df2a8fe
Josh Trebilco <bitbucket@trebilco.net>
2023-04-20 19:11:01 +1200

    Update MentorFactory.php

--- database/factories/MentorFactory.php
+++ database/factories/MentorFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Mentor>
  */
 class MentorFactory extends Factory
 {
