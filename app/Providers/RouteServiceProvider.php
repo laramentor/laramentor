21ed0adaf217898e5edddc8abe395def9a01d4bd
Josh Trebilco <bitbucket@trebilco.net>
2023-04-21 10:59:47 +1200

    refactor auth routes and move to route service provider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -32,6 +32,9 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
+                ->group(base_path('routes/auth.php'));
+
+            Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
     }
