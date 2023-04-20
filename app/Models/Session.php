86168c99180a5230ad5b83a3e2433b365ac0961d
Josh Trebilco <bitbucket@trebilco.net>
2023-04-20 19:12:51 +1200

    implement functions on session model to manage google calendar events and meetings

--- app/Models/Session.php
+++ app/Models/Session.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Services\GoogleCalendarEventService;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -25,5 +26,54 @@ class Session extends Model
     public function mentor(): BelongsTo
     {
         return $this->belongsTo(Mentor::class);
+    }
+
+    public function createMeeting()
+    {
+        $event = new GoogleCalendarEventService;
+        $event->name = $this->name;
+        $event->description = $this->description;
+        $event->startDateTime = $this->start_date_time;
+        $event->endDateTime = $this->end_date_time;
+        $event->addAttendee([
+            'email' => $this->user->email,
+        ]);
+        $event->addAttendee([
+            'email' => $this->mentor->user->email,
+        ]);
+        $event->addMeetLink();
+
+        $newEvent = $event->save();
+
+        $this->event_id = $newEvent->googleEvent->id;
+        $this->meeting_link = $newEvent->googleEvent->hangoutLink;
+        $this->save();
+    }
+
+    public function updateMeeting()
+    {
+        $event = GoogleCalendarEventService::find($this->event_id);
+        $event->name = $this->name;
+        $event->description = $this->description;
+        $event->startDateTime = $this->start_date_time;
+        $event->endDateTime = $this->end_date_time;
+        $event->addAttendee([
+            'email' => $this->user->email,
+        ]);
+        $event->addAttendee([
+            'email' => $this->mentor->user->email,
+        ]);
+
+        $event->save();
+    }
+
+    public function deleteMeeting()
+    {
+        $event = GoogleCalendarEventService::find($this->event_id);
+        $event->delete($event->id);
+
+        $this->event_id = null;
+        $this->meeting_link = null;
+        $this->save();
     }
 }
