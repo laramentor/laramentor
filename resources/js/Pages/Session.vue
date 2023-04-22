384aa4d0b8340d30ddf4e7c8ca2b787862493a5c
Josh Trebilco <bitbucket@trebilco.net>
2023-04-22 18:18:03 +1200

    Update Session.vue

--- resources/js/Pages/Session.vue
+++ resources/js/Pages/Session.vue
@@ -1,7 +1,6 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
-import { defineProps } from 'vue';
 
 const props = defineProps({
     session: {
